e78fd5db8f98f1693e08a39a0172a7a17dbe0e34
mun <quangnd.edu@gmail.com>
2019-04-11 18:20:40 +0700

    First commit

---
+++ config.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  url: 'https://quangnd.com',
+  title: 'Blog by Quang Nguyen',
+  subtitle: 'Trust, but verify.',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: '',
+    photo: '/photo.jpg',
+    bio: 'Bạn không bao giờ có thể đạt được sự hoàn hảo, nhưng bạn có thể tin vào đường tiệm cận của những gì mình không ngừng hướng tới.📚',
+    contacts: {
+      email: '#',
+      telegram: '#',
+      twitter: '#',
+      github: '#',
+      rss: '#',
+      vkontakte: '#'
+    }
+  }
+};
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,71 @@
+// @flow
+import React from 'react';
+import { graphql, StaticQuery } from 'gatsby';
+import Author from './Author';
+import Contacts from './Contacts';
+import Copyright from './Copyright';
+import Menu from './Menu';
+import styles from './Sidebar.module.scss';
+
+type Props = {
+  +isIndex: ?boolean,
+};
+
+type PureProps = Props & {
+  +data: Object,
+};
+
+export const PureSidebar = ({ data, isIndex }: PureProps) => {
+  const {
+    author,
+    copyright,
+    menu
+  } = data.site.siteMetadata;
+
+  return (
+    <div className={styles['sidebar']}>
+      <div className={styles['sidebar__inner']}>
+        <Author author={author} isIndex={isIndex} />
+        <Menu menu={menu} />
+        <Contacts contacts={author.contacts} />
+        <Copyright copyright={copyright} />
+      </div>
+    </div>
+  );
+};
+
+export const Sidebar = (props: Props) => (
+  <StaticQuery
+    query={graphql`
+      query SidebarQuery {
+        site {
+          siteMetadata {
+            title
+            subtitle
+            copyright
+            menu {
+              label
+              path
+            }
+            author {
+              name
+              photo
+              bio
+              contacts {
+                twitter
+                telegram
+                github
+                email
+                rss
+                vkontakte
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={(data) => <PureSidebar {...props} data={data}/>}
+  />
+);
+
+export default Sidebar;
